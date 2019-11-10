2951d1949ae4e5ba926b68fafb17ba1a856fcf0c
Felipe <felipe.pucsc@gmail.com>
2019-11-10 17:53:02 -0300

    Creating proejct]

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Nubank Home Screen',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+    );
+  }
+}
